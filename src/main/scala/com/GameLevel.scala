e58b8963a634568ad8f866637c0fabf71f41cc46
Martin Ahmadzadeh <martin.ahmadzadeh@dominionmarinemedia.com>
2015-05-21 23:04:22 -0700

    minor change

--- src/main/scala/com/GameLevel.scala
+++ src/main/scala/com/GameLevel.scala
@@ -2,11 +2,17 @@ package com
 
 case class GameLevel(shapeCount:Int ) {
 
-    def nextLevel: GameLevel = if(shapeCount == 36 ) this else GameLevel(shapeCount + 1 )
+    def nextLevel: GameLevel =
+        if( hasReachedMaxLevel)
+            this
+        else GameLevel( shapeCount + 1 )
 
     // for now the points for each level is equal to the
     // shape count presented in that level.
     def points   = shapeCount
+
+    private def hasReachedMaxLevel: Boolean =
+        shapeCount == DisplayGrid.GRID_COL_CNT * DisplayGrid.GRID_COL_CNT
 
     override def toString:String = s"${shapeCount}"
 }
